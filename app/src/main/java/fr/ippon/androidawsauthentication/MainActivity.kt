770df15effc4547a3594135984b7a971dfb443d7
Lebret Audrey <alebret@ippon.fr>
2020-04-06 13:15:35 +0200

    Add Aws authentication page

--- app/src/main/java/fr/ippon/androidawsauthentication/MainActivity.kt
+++ app/src/main/java/fr/ippon/androidawsauthentication/MainActivity.kt
@@ -1,11 +1,10 @@
 package fr.ippon.androidawsauthentication
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
 
