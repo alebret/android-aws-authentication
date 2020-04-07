76b5f66bba537cda8075a1321cae974931b72ded
Lebret Audrey <alebret@ippon.fr>
2020-04-07 12:43:13 +0200

    Add Aws authentication custom page, only UI

---
+++ app/src/main/java/fr/ippon/androidawsauthentication/AuthenticationActivity.kt
@@ -0,0 +1,19 @@
+package fr.ippon.androidawsauthentication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_authentication.*
+
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+
+        buttonLogin.setOnClickListener {
+            //TODO
+        }
+    }
+
+}
--- app/src/main/java/fr/ippon/androidawsauthentication/FirstFragment.kt
+++ app/src/main/java/fr/ippon/androidawsauthentication/FirstFragment.kt
@@ -1,12 +1,13 @@
 package fr.ippon.androidawsauthentication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,8 +15,8 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -26,6 +27,10 @@ class FirstFragment : Fragment() {
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        buttonLogout.setOnClickListener {
+            //TODO
         }
     }
 }
